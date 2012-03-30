db6f21c37e192eb15f61dd544cce11eec9cb915a
Tom Barber <tom@analytical-labs.com>
2012-03-30 15:10:58 +0100

    files

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'AnalyticalLabs-nfs'
+version '1.0'
+
+author 'Tom Barber'
+license ''
+project_page ''
+source ''
+summary ''
+description ''
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: nfs
+#
+# This module manages nfs
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class nfs {
+
+
+}
